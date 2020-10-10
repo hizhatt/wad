d80604e114c2afdba1e7ad4ce9af92010d7993ef
hizhatt <32994801+hizhatt@users.noreply.github.com>
2020-10-11 00:28:36 +0800

    made header and footer php

---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="topnav">
+	<a href="index.php">Home</a>
+	<a href="Recent.html">Recent</a>
+	<a href="Popular.html">Popular</a>
+	<a href="Genre.html">Genre</a>
+	<a href="About.html">About Us</a>
+	<div class="dropdown">
+		<button class="dropbtn">Account
+			<i class="fa fa-caret-down"></i>
+		</button>
+		<div class="dropdown-content">
+			<a href="AccountDetails.html">Account Details</a>
+			<a href="Order.html">Order</a>
+		</div>
+	</div>
--- index.php
+++ index.php
@@ -15,25 +15,13 @@
 	
 	
 <body>
-
+	<header>
+		<?php include"header.php" ?>
+	</header>
 	
     <div class="content">
 		
-		<div class="topnav">
-            <a href="index.php">Home</a>
-            <a href="Recent.html">Recent</a>
-            <a href="Popular.html">Popular</a>
-            <a href="Genre.html">Genre</a>
-            <a href="About.html">About Us</a>
-            <div class="dropdown">
-    			<button class="dropbtn">Account
-      				<i class="fa fa-caret-down"></i>
-    			</button>
-    			<div class="dropdown-content">
-      				<a href="AccountDetails.html">Account Details</a>
-      				<a href="Order.html">Order</a>
-    			</div>
-  			</div>
+	
             
             
             <form class="example" action="/action_page.php" style="margin:auto;max-width:300px">
