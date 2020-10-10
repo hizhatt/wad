d8862ae5f03800a6114d49bb2c0e190568960d15
hizhatt <32994801+hizhatt@users.noreply.github.com>
2020-10-11 00:43:15 +0800

    Update php all

---
+++ About.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>About</title>
+	<link rel="stylesheet" href="css/top_nav.css">
+	<link rel="stylesheet" href="css/about_us.css">
+</head>
+<body>
+	<header>
+		<?php include"header.php" ?>
+	</header>
+	
+	<div class="content">
+		
+
+
+	<h2 style="text-align:center">Our Team</h2>
+	<div class="row">
+	  <div class="column">
+		<div class="card">
+		  <img src="images/aina.jpeg" style="width: 75%" >
+		  <div class="container">
+			<h2 style="text-align: center">Aina Nadhirah</h2>
+			<p class="title">Web Designer</p>
+			<p>1161302020@student.mmu.edu.my</p>
+			<p><button onclick="location.href='mailto:1161302020@student.mmu.edu.my';" class="button">Contact</button></p>
+		  </div>
+		</div>
+	  </div>
+
+	  <div class="column">
+		<div class="card">
+		  <img src="Images/adam.jpg" alt="Mike" style="width:100%">
+		  <div class="container">
+			<h2>Aiman Adam</h2>
+			<p class="title">CEO</p>
+			<p>1171303266@student.mmu.edu.my</p>
+			<p><button onclick="location.href='mailto:1171303266@student.mmu.edu.my';" class="button">Contact</button></p>
+		  </div>
+		</div>
+	  </div>
+
+	  <div class="column">
+		<div class="card">
+		  <img src="Images/hizri.jpg" alt="Mike" style="width:100%">
+		  <div class="container">
+			<h2>Hizri Hatta</h2>
+			<p class="title">Supervisor</p>
+			<p>1181300659@student.mmu.edu.my</p>
+			<p><button onclick="location.href='mailto:1181300659@student.mmu.edu.my';" class="button">Contact</button></p>
+		  </div>
+		</div>
+	  </div>
+
+	  <div class="column">
+		<div class="card">
+		  <img src="Images/syarif.jpg" alt="Mike" style="width:100%">
+		  <div class="container">
+			<h2>Syarif Azmin</h2>
+			<p class="title">Tester</p>
+			<p>1171303617@student.mmu.edu.my</p>
+			<p><button onclick="location.href='mailto:1171303617@student.mmu.edu.my';" class="button">Contact</button></p>
+		  </div>
+		</div>
+	  </div>
+
+
+	  
+	  
+	  </div>
+	</div>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <div class="topnav">
 	<a href="index.php">Home</a>
-	<a href="Recent.html">Recent</a>
-	<a href="Popular.html">Popular</a>
-	<a href="Genre.html">Genre</a>
-	<a href="About.html">About Us</a>
+	<a href="Recent.php">Recent</a>
+	<a href="Popular.php">Popular</a>
+	<a href="Genre.php">Genre</a>
+	<a href="About.php">About Us</a>
 	<div class="dropdown">
 		<button class="dropbtn">Account
 			<i class="fa fa-caret-down"></i>
 		</button>
 		<div class="dropdown-content">
-			<a href="AccountDetails.html">Account Details</a>
-			<a href="Order.html">Order</a>
+			<a href="AccountDetails.php">Account Details</a>
+			<a href="Order.php">Order</a>
 		</div>
 	</div>
