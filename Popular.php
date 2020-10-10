329790560ee7c36b6812d92213046da42fcf44ed
hizhatt <32994801+hizhatt@users.noreply.github.com>
2020-10-11 00:48:18 +0800

    Cleaned up all PHP stuff

--- Popular.php
+++ Popular.php
@@ -3,6 +3,11 @@
 <head>
 <meta charset="utf-8">
 <title>Popular</title>
+        <link rel="stylesheet" href="css/top_nav.css">
+        <link rel="stylesheet" href="css/home_page.css">
+        <!--<link rel="stylesheet" href="login_form.css">-->
+    
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
