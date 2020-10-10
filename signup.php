4bac9719359f811ff9e4e18f51cc42e44358626a
Kamcoi <21355197+kamcoi@users.noreply.github.com>
2020-10-11 02:01:38 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,7 +1,9 @@
 <!-- The Modal (contains the Sign Up form) -->
+<?php include('server.php') ?>
 		<div id="id02" class="modal">
   			
-  			<form class="modal-content animate" action="/action_page.php">
+  			<form method="post" class="modal-content animate" action="signup.php">
+			<?php include('errors.php'); ?>
   			<span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
     		<div class="container">
      			
@@ -9,16 +11,16 @@
       			<p>Please fill in this form to create an account.</p>
       			<hr>
       			<label for="email"><b>Email</b></label>
-      			<input type="text" placeholder="Enter Email" name="email" required>
+      			<input type="text" placeholder="Enter Email" name="email" value="<?php echo $email; ?>" required>
       			
-      			<label for="uname"><b>Username</b></label>
-      			<input type="text" placeholder="Enter Username" name="uname" required>
+      			<label for="username"><b>Username</b></label>
+      			<input type="text" placeholder="Enter Username" name="username" value="<?php echo $username; ?>" required>
 
-      			<label for="psw"><b>Password</b></label>
-      			<input type="password" placeholder="Enter Password" name="psw" required>
+      			<label for="password"><b>Password</b></label>
+      			<input type="password" placeholder="Enter Password" name="password_1" required>
 
-      			<label for="psw-repeat"><b>Confirm Password</b></label>
-      			<input type="password" placeholder="Confirm Password" name="psw-repeat" required>
+      			<label for="password_2"><b>Confirm Password</b></label>
+      			<input type="password" placeholder="Confirm Password" name="password_2" required>
 
       			<label>
         			<input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
@@ -28,7 +30,7 @@
 
       			<div class="clearfix">
       				<button type="button" class="cancelbtn">Cancel</button>
-      				<button type="submit" class="signupbtn">Sign Up</button>
+      				<button type="submit" class="signupbtn" name="reg_user">Sign Up</button>
     			</div>
     			</div>
   				</form>
