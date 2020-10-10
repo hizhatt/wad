c20c9f28985cb9747e59d96a2b98e5729cd48793
nadhirania <43725107+nadhirania@users.noreply.github.com>
2020-10-11 03:26:49 +0800

    temp order

--- Order.php
+++ Order.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Order</title>
-        <link rel="stylesheet" href="css/top_nav.css">
-        <link rel="stylesheet" href="css/home_page.css">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>About</title>
+	<link rel="stylesheet" href="css/top_nav.css">
+	<link rel="stylesheet" href="css/home_page.css">
+	<link rel="stylesheet" href="css/order.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -26,22 +26,20 @@
 			<hr>
 				
 		</div>
-		
-		<table class="w3-table-all w3-hoverable">
-    		<thead>
-      			<tr class="w3-indigo">
-        			<th>Orders</th>
-        			<th>Date</th>
-        			<th>Status</th>
-      			</tr>
-    		</thead>
-    		<tr class="w3-hover-pink">
-      			<td>Order ID</td>
+	
+  		<table>
+  			<tr>
+    			<th>Orders</th>
+        		<th>Date</th>
+        		<th>Status</th>
+  			</tr>
+  			<tr>
+    			<td>Order ID</td>
       			<td>dd-mm-yy</td>
       			<td>Status</td>
-    		</tr>
-    
-  		</table>
+  			</tr>
+  
+		</table>
 		
 	</form>
 	
