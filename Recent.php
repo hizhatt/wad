9e454c57702bcaa2afbafd591a89f9450566c6d1
syarifazmin <babysyarif@gmail.com>
2020-10-11 12:36:33 +0800

    recent

--- Recent.php
+++ Recent.php
@@ -15,6 +15,22 @@
 	<header>
 		<?php include"header.php" ?>
 	</header>
-	
+        <br>
+	<br>
+	<div class="MiddleContent">
+		
+		<div class="row">
+			
+			<div class="column">
+              	<a href="Anime1.php">
+                  	<img src="Images/Anime1.jpg" alt="anime1" style="width: 50%; height:50% ">
+              	</a>
+              	<p class="title">Fullmetal Alchemist: Brotherhood</p>
+           		
+              	<p class="italic">2009 | 64 Episodes</p>
+              
+			</div>
+		</div>
+	</div>
 </body>
 </html>
