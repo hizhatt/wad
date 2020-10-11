286166f36f4618920901a2522e1d21a3c91bdc80
nadhirania <43725107+nadhirania@users.noreply.github.com>
2020-10-11 13:32:25 +0800

    genre.php edited

--- Genre.php
+++ Genre.php
@@ -5,8 +5,7 @@
 <title>Genre</title>
         <link rel="stylesheet" href="css/top_nav.css">
         <link rel="stylesheet" href="css/home_page.css">
-        <!--<link rel="stylesheet" href="login_form.css">-->
-    
+    	<link rel="stylesheet" href="css/genre.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
@@ -15,6 +14,17 @@
 	<header>
 		<?php include"header.php" ?>
 	</header>
+	<br><br>
+	<div class="MiddleContent">
+		
+		<p id="rcorners1">Action</p>
+
+		<p id="rcorners2">Adventure</p>
+
+		<p id="rcorners3">Fantasy</p>
+		
+		<p id="rcorners4">Romance</p>
+	</div>
 	
 </body>
 </html>
