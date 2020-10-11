1ec97ad69a7de8e074df7eef3c4ddf48db2128b3
nadhirania <43725107+nadhirania@users.noreply.github.com>
2020-10-11 15:58:05 +0800

    edited genre.php

--- Genre.php
+++ Genre.php
@@ -18,12 +18,152 @@
 	<div class="MiddleContent">
 		
 		<p id="action">Action</p>
+		<br>
+		<div class="row">
+			
+			<div class="column">
+                <img src="Images/Anime1.jpg" alt="anime1" style="width: 50%; height:50% ">
+              	<p class="title">Fullmetal Alchemist:Brotherhood</p>
+           		<p class="italic">2009 | 64 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/Anime2.jpg" alt="anime2" style="width: 50%; height:50% ">
+              	<p class="title">Hunter X Hunter</p>
+           		<p class="italic">2011 | 148 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/popular4.jpg" alt="popular4" style="width: 50%; height:50% ">
+              	<p class="title">Tokyo Ghoul</p>
+           		<p class="italic">2014 | 48 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/Anime4.jpg" alt="anime4" style="width: 50%; height:50% ">
+              	<p class="title">Gintama'</p>
+           		<p class="italic">2006 | 367 Episodes + OVAs</p>
+			</div>
+			
+			<div class="row">
+			
+				<<div class="column">
+					<img src="Images/Anime5.jpg" alt="anime5" style="width: 50%; height:50% ">
+              		<p class="title">Death Note</p>
+           			<p class="italic">2006 | 37 Episodes</p>
+				</div>
+			
+				<div class="column">
+                	<img src="Images/recent1.jpg" alt="recent1" style="width: 50%; height:50% ">
+              		<p class="title">Cannon Busters</p>
+           			<p class="italic">2019 | 12 Episodes</p>
+				</div>
+			
+				<div class="column">
+                	<img src="Images/Anime1.jpg" alt="anime1" style="width: 50%; height:50% ">
+              		<p class="title">Fullmetal Alchemist:Brotherhood</p>
+           			<p class="italic">2009 | 64 Episodes</p>
+				</div>
+				
+				<div class="column">
+					<img src="Images/Anime3.jpg" alt="anime3" style="width: 50%; height:50% ">
+              		<p class="title">Steins;Gate</p>
+           			<p class="italic">2011 | 24 Episodes</p>
+				</div>
+			
+				
+			</div>
 
-		<p id="adventure">Adventure</p>
-
-		<p id="fantasy">Fantasy</p>
+		</div>
 		
+		<br>
+		<p id="adventure">Adventure</p>
+		<br>
+		<div class="row">
+			<div class="column">
+				<img src="Images/Anime8.jpg" alt="anime8" style="width: 50%; height:50% ">
+              	<p class="title">Fairy Tail</p>
+           		<p class="italic">2018 | 13 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/recent8.jpg" alt="recent8" style="width: 50%; height:50% ">
+              	<p class="title">Japan Sinks 2020</p>
+           		<p class="italic">2020 | 10 Episodes</p>
+			</div>
+			
+			<div class="column">
+					<img src="Images/Anime7.jpg" alt="anime7" style="width: 50%; height:50% ">
+              		<p class="title">Re:Zero</p>
+           			<p class="italic">2016 | 38 Episodes</p>
+			</div>
+			
+			<div class="column">
+					<img src="Images/recent2.jpg" alt="recent2" style="width: 50%; height:50% ">
+              		<p class="title">The Rising of The Shield Hero</p>
+           			<p class="italic">2019 | 25 Episodes</p>
+			</div>
+			
+		</div>
+		<br><br>
+		<p id="fantasy">Fantasy</p>
+		<br>
+		<div class="row">
+		
+			<div class="column">
+				<img src="Images/recent4.jpg" alt="recent4" style="width: 50%; height:50% ">
+              	<p class="title">Lost Song</p>
+           		<p class="italic">2018 | 12 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/recent5.jpg" alt="recent5" style="width: 50%; height:50% ">
+              	<p class="title">Last Hope</p>
+           		<p class="italic">2018 | 26 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/popular2.jpg" alt="popular2" style="width: 50%; height:50% ">
+              	<p class="title">Kakegurui</p>
+           		<p class="italic">2017 | 24 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/popular1.jpg" alt="popular1" style="width: 50%; height:50% ">
+              	<p class="title">No Game No Life</p>
+           		<p class="italic">2014 | 12 Episodes</p>
+			</div>
+			
+		</div>
+		<br><br>
 		<p id="romance">Romance</p>
+		
+		<div class="row">
+			<div class="column">
+				<img src="Images/Anime6.jpg" alt="anime6" style="width: 50%; height:50% ">
+              	<p class="title">Fruit Basket</p>
+           		<p class="italic">2019 | 50 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/recent6.jpg" alt="recent6" style="width: 50%; height:50% ">
+              	<p class="title">Violet Evergarden</p>
+           		<p class="italic">2018 | 13 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/recent4.jpg" alt="recent4" style="width: 50%; height:50% ">
+              	<p class="title">Lost Song</p>
+           		<p class="italic">2018 | 12 Episodes</p>
+			</div>
+			
+			<div class="column">
+				<img src="Images/genre1.jpg" alt="genre1" style="width: 50%; height:50% ">
+              	<p class="title">Sword Art Online</p>
+           		<p class="italic">2012 | 47 Episodes</p>
+			</div>
+		
+		</div>
 	</div>
 	
 </body>
