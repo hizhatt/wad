fe77486a9fccbd4b63b63cd1e09fd81787922357
hizhatt <32994801+hizhatt@users.noreply.github.com>
2020-10-11 15:55:21 +0800

    clickable anime thumbnails

--- Popular.php
+++ Popular.php
@@ -24,25 +24,25 @@
 		<div class="row">
 			
 			<div class="column">
-                <img src="Images/Anime1.jpg" alt="anime1" style="width: 50%; height:50% ">
+                <a href="Anime.php?id=1"><img src="Images/Anime1.jpg" alt="anime1" style="width: 50%; height:50% "><a>
               	<p class="title">Fullmetal Alchemist:Brotherhood</p>
            		<p class="italic">2009 | 64 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/Anime5.jpg" alt="anime5" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=2"><img src="Images/Anime5.jpg" alt="anime5" style="width: 50%; height:50% "><a>
               	<p class="title">Death Note</p>
            		<p class="italic">2006 | 37 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/Anime8.jpg" alt="anime8" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=3"><img src="Images/Anime8.jpg" alt="anime8" style="width: 50%; height:50% "><a>
               	<p class="title">Fairy Tail</p>
            		<p class="italic">2018 | 13 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/popular1.jpg" alt="popular1" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=4"><img src="Images/popular1.jpg" alt="popular1" style="width: 50%; height:50% "><a>
               	<p class="title">No Game No Life</p>
            		<p class="italic">2014 | 12 Episodes</p>
 			</div>
@@ -51,25 +51,25 @@
 		
 		<div class="row">
 			<div class="column">
-				<img src="Images/Anime7.jpg" alt="anime7" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=5"><img src="Images/Anime7.jpg" alt="anime7" style="width: 50%; height:50% "><a>
               	<p class="title">Re:Zero</p>
            		<p class="italic">2016 | 38 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/popular2.jpg" alt="popular2" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=6"><img src="Images/popular2.jpg" alt="popular2" style="width: 50%; height:50% "><a>
               	<p class="title">Kakegurui</p>
            		<p class="italic">2017 | 24 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent7.jpg" alt="recent7" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=7"><img src="Images/recent7.jpg" alt="recent7" style="width: 50%; height:50% "><a>
               	<p class="title">B:The Beginning</p>
            		<p class="italic">2018 | 12 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent8.jpg" alt="recent8" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=8"><img src="Images/recent8.jpg" alt="recent8" style="width: 50%; height:50% "><a>
               	<p class="title">Japan Sinks 2020</p>
            		<p class="italic">2020 | Movie</p>
 			</div>
@@ -77,25 +77,25 @@
 		
 		<div class="row">
 			<div class="column">
-				<img src="Images/recent4.jpg" alt="recent4" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=1"><img src="Images/recent4.jpg" alt="recent4" style="width: 50%; height:50% "><a>
               	<p class="title">Lost Song</p>
            		<p class="italic">2018 | 12 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/popular3.jpg" alt="popular3" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=2"><img src="Images/popular3.jpg" alt="popular3" style="width: 50%; height:50% "><a>
               	<p class="title">Attack on Titan</p>
            		<p class="italic">2009 | 59 Episodes + OVAs</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/popular4.jpg" alt="popular4" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=3"><img src="Images/popular4.jpg" alt="popular4" style="width: 50%; height:50% "><a>
               	<p class="title">Tokyo Ghoul</p>
            		<p class="italic">2014 | 48 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/Anime3.jpg" alt="anime3" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=4"><img src="Images/Anime3.jpg" alt="anime3" style="width: 50%; height:50% "><a>
               	<p class="title">Steins;Gate</p>
            		<p class="italic">2011 | 24 Episodes</p>
 			</div>
--- Recent.php
+++ Recent.php
@@ -24,25 +24,25 @@
 		<div class="row">
 			
 			<div class="column">
-                <img src="Images/recent1.jpg" alt="recent1" style="width: 50%; height:50% ">
+                <a href="Anime.php?id=1"><img src="Images/recent1.jpg" alt="recent1" style="width: 50%; height:50% "></a>
               	<p class="title">Cannon Busters</p>
            		<p class="italic">2019 | 12 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent2.jpg" alt="recent2" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=2"><img src="Images/recent2.jpg" alt="recent2" style="width: 50%; height:50% "></a>
               	<p class="title">The Rising of The Shield Hero</p>
            		<p class="italic">2019 | 25 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent3.jpg" alt="recent3" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=3"><img src="Images/recent3.jpg" alt="recent3" style="width: 50%; height:50% "></a>
               	<p class="title">Hanebado!</p>
            		<p class="italic">2018 | 13 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent4.jpg" alt="recent4" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=4"><img src="Images/recent4.jpg" alt="recent4" style="width: 50%; height:50% "></a>
               	<p class="title">Lost Song</p>
            		<p class="italic">2018 | 12 Episodes</p>
 			</div>
@@ -51,25 +51,25 @@
 		
 		<div class="row">
 			<div class="column">
-				<img src="Images/recent5.jpg" alt="recent5" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=5"><img src="Images/recent5.jpg" alt="recent5" style="width: 50%; height:50% "></a>
               	<p class="title">Last Hope</p>
            		<p class="italic">2018 | 26 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent6.jpg" alt="recent6" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=6"><img src="Images/recent6.jpg" alt="recent6" style="width: 50%; height:50% "></a>
               	<p class="title">Violet Evergarden</p>
            		<p class="italic">2018 | 13 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent7.jpg" alt="recent7" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=7"><img src="Images/recent7.jpg" alt="recent7" style="width: 50%; height:50% "></a>
               	<p class="title">B:The Beginning</p>
            		<p class="italic">2018 | 12 Episodes</p>
 			</div>
 			
 			<div class="column">
-				<img src="Images/recent8.jpg" alt="recent8" style="width: 50%; height:50% ">
+				<a href="Anime.php?id=8"><img src="Images/recent8.jpg" alt="recent8" style="width: 50%; height:50% "></a>
               	<p class="title">Japan Sinks 2020</p>
            		<p class="italic">2020 | Movie</p>
 			</div>
