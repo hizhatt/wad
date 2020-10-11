569381b95827a770ff708201b5e89865744d8ec2
Kamcoi <21355197+kamcoi@users.noreply.github.com>
2020-10-11 12:25:36 +0800

    hotfix

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 	<?php endif ?>
 
 	<?php  if (isset($_SESSION['username'])) : ?>
-		<a><i class="fa fa-id-badge" style="font-size:24px; float:right; width: auto;">Welcome <strong><?php echo $_SESSION['username']; ?></strong></i></a>
+		<a><i class="fa fa-id-badge" style="font-size:24px; float:right; width: auto;">Welcome  <strong><?php echo $_SESSION['username']; ?></strong></i></a>
 		<button onclick="location.href='index.php?logout=1'" style=" font-size:22px; width:auto; float:right; margin-right: 1%; background-color: #990000">Logout</button>
 	<?php endif ?>
 
