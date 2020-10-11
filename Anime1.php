aca17120917f698a171a702c9b574255e32c1544
nadhirania <43725107+nadhirania@users.noreply.github.com>
2020-10-11 11:20:55 +0800

    edited popular.php

--- Anime1.php
+++ Anime1.php
@@ -7,7 +7,9 @@
     <head>
           
         <link rel="stylesheet" href="css/top_nav.css">
+        <link rel="stylesheet" href="css/home_page.css">
         <link rel="stylesheet" href="css/AnimePage.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
      
     </head>
 	<body>
